13d4eecad7cb0d3530a8c09ecb621b2526b9c72c
Jeremy Muhia <jmuhia80@gmail.com>
2016-10-24 17:20:39 -0400

    add edits to view controller class

--- iOS Programming 24 Oct - App Activity/ViewController.m
+++ iOS Programming 24 Oct - App Activity/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIScrollView * scrollView;
+    UIView * containerView;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +20,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    scrollView = [[UIScrollView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    scrollView.bouncesZoom = YES;
+    scrollView.backgroundColor = [UIColor blackColor];
+    
+    containerView = [[UIView alloc] initWithFrame:CGRectZero];
+    [scrollView addSubview:containerView];
+    
+    CGFloat maxWidth = 0.0;
+    CGFloat totalHeight = 0;
+    
+    for (int index = 0; index < 4; index++) {
+        UIImage * img = [UIImage imageNamed:[NSString stringWithFormat:@"%dpix.jpg", index]];
+        
+        UIImageView * imageView;
+        
+        [containerView addSubview:imageView];
+    }
 }
 
 
